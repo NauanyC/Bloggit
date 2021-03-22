3166b2ce1493659d855be13f29704110fd081c1c
Nau Nau <nauanycc@outlook.com.br>
2021-03-22 15:08:55 -0300

    feat: fetch blogs

--- src/pages/postsList.tsx
+++ src/pages/postsList.tsx
@@ -1,8 +1,37 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+//Interfaces
+import { Blog } from "../interfaces/Blog";
 
 export interface PostsListProps {}
 
 const PostsList: React.SFC<PostsListProps> = () => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    setError("");
+    setLoading(true);
+
+    axios
+      .get<Blog[]>("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setBlogs(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        const error =
+          ex.response.status === 404
+            ? "Resource not found"
+            : "An unexpected error has occurred";
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="PostsList">
       <h1>PostsList</h1>
